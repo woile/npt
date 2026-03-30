866129c18059a1014874947224ab313a5c37206d
Santiago Fraire Willemoes <santiwilly@gmail.com>
2026-03-30 09:06:59 +0100

    fix: update flake

--- flake.nix
+++ flake.nix
@@ -1,28 +1,72 @@
 {
+  description = "A rust flake";
   inputs = {
-    naersk.url = "github:nix-community/naersk/master";
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    utils.url = "github:numtide/flake-utils";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    crane = {
+      url = "github:ipetkov/crane";
+    };
   };
+  outputs =
+    inputs@{
+      flake-parts,
+      crane,
+      ...
+    }:
+    # https://flake.parts/
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+      perSystem =
+        {
+          pkgs,
+          inputs',
+          self',
+          ...
+        }:
+        let
+          rustChannel = "stable";
+          fenix = inputs'.fenix.packages;
+          rustToolchain = (
+            fenix.combine [
+              fenix.${rustChannel}.toolchain
 
-  outputs = { self, nixpkgs, utils, naersk }:
-    utils.lib.eachDefaultSystem (system:
-      let
-        cargoToml = (builtins.fromTOML (builtins.readFile ./Cargo.toml));
-        pkgs = import nixpkgs { inherit system; };
-        naersk-lib = pkgs.callPackage naersk { };
-      in
-      {
-        packages.npt = naersk-lib.buildPackage ./.;
-        defaultPackage = naersk-lib.buildPackage ./.;
+              # https://doc.rust-lang.org/rustc/platform-support.html
+              # For more targets add:
+              # fenix.targets.aarch64-linux-android."${rustChannel}".rust-std
+              # fenix.targets.x86_64-linux-android."${rustChannel}".rust-std
+            ]
+          );
+          craneLib = (crane.mkLib pkgs).overrideToolchain rustToolchain;
+        in
+        {
+          # Build the binary package with:
+          packages.npt = craneLib.buildPackage {
+            src = ./.;
+          };
+          # nix build .
+          packages.default = self'.packages.npt;
 
-        devShell = with pkgs; mkShell {
-          buildInputs = [ cargo rustc rustfmt pre-commit rustPackages.clippy ];
-          RUST_SRC_PATH = rustPlatform.rustLibSrc;
-          shellHook = ''
-          $SHELL
-          '';
+          # Default shell opened with `nix develop`
+          devShells.default = pkgs.mkShell {
+            name = "dev";
+
+            # Available packages on https://search.nixos.org/packages
+            buildInputs = with pkgs; [
+              just
+              rustToolchain
+            ];
+
+            shellHook = ''
+              echo "Welcome to the rust devshell!"
+            '';
+          };
         };
-
-      });
+    };
 }
